3d028b6ae768d00b0c60a7ff2849bedb108a8a45
wpierce19 <wpierce53@gmail.com>
2024-08-04 17:15:44 -0600

    Finished challenges 08-12

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let i=0;i <array.length;i++){
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let king = array[0];
+  for (let i=1;i<array.length;i++){
+    king = king * array[i];
+  }
+  return king;
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0){
+    return -1;
+  }
+  else if (num == 0){
+    return 1;
+  }
+  else {
+    return (num * factorial(num-1));
+  }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    // checks argument's type and makes sure we use 
+    // a number throughout rest of function.
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 
 };
-
-// Do not edit below this line
-module.exports = fibonacci;
